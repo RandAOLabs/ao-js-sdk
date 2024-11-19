39c6c4682c6d72696046a298485181c99f1a9091
K <k@k.com>
2024-11-18 20:42:43 -0500

    change access on base client

--- src/core/abstract/IBaseClient.ts
+++ src/core/abstract/IBaseClient.ts
@@ -4,10 +4,10 @@ import { ResultsResponse } from "@permaweb/aoconnect/dist/lib/results";
 import { Tags } from "./types";
 import { MessageResult } from "@permaweb/aoconnect/dist/lib/result";
 export abstract class IBaseClient {
-    abstract message(data?: string, tags?: Tags, anchor?: string): Promise<string>;
-    abstract results(from?: string, to?: string, limit?: number, sort?: string): Promise<ResultsResponse>;
-    abstract result(messageId: string): Promise<MessageResult>;
-    abstract dryrun(data: any, tags: Tags, anchor?: string, id?: string, owner?: string): Promise<any>;
+    protected abstract message(data?: string, tags?: Tags, anchor?: string): Promise<string>;
+    protected abstract results(from?: string, to?: string, limit?: number, sort?: string): Promise<ResultsResponse>;
+    protected abstract result(messageId: string): Promise<MessageResult>;
+    protected abstract dryrun(data: any, tags: Tags, anchor?: string, id?: string, owner?: string): Promise<any>;
     public static autoConfiguration(): IBaseClient {
         throw new Error("Method not implemented")
     }
