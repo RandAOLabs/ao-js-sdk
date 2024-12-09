f105db85add67bebf5985163c41b1f4348ef5c54
RandAO <aorandom@proton.me>
2024-12-09 11:27:59 -0500

    Update random process (#16)
    Co-authored-by: K <k@k.com>

--- src/clients/random/RandomClientAutoConfiguration.ts
+++ src/clients/random/RandomClientAutoConfiguration.ts
@@ -3,6 +3,6 @@ import { RandomClientConfig } from "./abstract/RandomClientConfig";
 
 export const getRandomClientAutoConfiguration = (): RandomClientConfig => ({
     ...getBaseClientAutoConfiguration(),
-    processId: "NlHgeFYslkUhiOdF1JcbTC9nkxFkwCJs8wjd2sBTjf8",
+    processId: "-3Nvdg7g9S7ly-2scR8TtcY3QmIwtl_Gx5PDREJssiA",
     tokenProcessId: "OeX1V1xSabUzUtNykWgu9GEaXqacBZawtK12_q5gXaA",
 });
