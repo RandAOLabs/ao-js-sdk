9331fe5ce7d38798d3a7c91374a394426fe872e0
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-01-24 14:01:21 -0500

    tests-wip

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 export * from "./core/index"
 export * from './clients/token/index';
 export * from './clients/random/index';
+export * from './clients/lootbox/index';
 export * from './utils/index'
