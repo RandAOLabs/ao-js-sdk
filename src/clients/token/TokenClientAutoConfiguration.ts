12f2b1fd3f4d93e5da5271a26ff868ad430ab446
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-01-26 17:38:37 -0500

    import

--- src/clients/token/TokenClientAutoConfiguration.ts
+++ src/clients/token/TokenClientAutoConfiguration.ts
@@ -1,5 +1,6 @@
 import { TokenClientConfig } from "./abstract/TokenClientConfig";
 import { getBaseClientAutoConfiguration, } from "../../core/BaseClientAutoConfiguration";
+import { RNG_TOKEN_PROCESS_ID } from "src/processes_ids";
 
 export const getTokenClientAutoConfiguration = (): TokenClientConfig => ({
     ...getBaseClientAutoConfiguration(),
--- src/processes_ids.ts
+++ src/processes_ids.ts
@@ -1,2 +1,2 @@
-const RNG_TOKEN_PROCESS_ID: string = "5ZR9uegKoEhE9fJMbs-MvWLIztMNCVxgpzfeBVE3vqI"
-const RANDOM_PROCESS_ID: string = "KbaY8P4h9wdHYKHlBSLbXN_yd-9gxUDxSgBackUxTiQ"
+export const RNG_TOKEN_PROCESS_ID: string = "5ZR9uegKoEhE9fJMbs-MvWLIztMNCVxgpzfeBVE3vqI"
+export const RANDOM_PROCESS_ID: string = "KbaY8P4h9wdHYKHlBSLbXN_yd-9gxUDxSgBackUxTiQ"
