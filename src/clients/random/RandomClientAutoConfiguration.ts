bb081e4383d12966d59a6d3df4bb1c3713556a61
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-01-26 17:38:37 -0500

    import

--- src/clients/random/RandomClientAutoConfiguration.ts
+++ src/clients/random/RandomClientAutoConfiguration.ts
@@ -1,7 +1,6 @@
 import { getBaseClientAutoConfiguration } from "../../core/BaseClientAutoConfiguration";
-import { getTokenClientAutoConfiguration } from "../token";
 import { RandomClientConfig } from "./abstract/RandomClientConfig";
-
+import { RANDOM_PROCESS_ID, RNG_TOKEN_PROCESS_ID } from "../../processes_ids"
 export const getRandomClientAutoConfiguration = (): RandomClientConfig => ({
     ...getBaseClientAutoConfiguration(),
     processId: RANDOM_PROCESS_ID,
