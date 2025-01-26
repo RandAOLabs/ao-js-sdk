9e01896ac0d89a48c6956e8a68b051d0d6935811
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-01-26 17:29:19 -0500

    eee

--- src/clients/random/RandomClientAutoConfiguration.ts
+++ src/clients/random/RandomClientAutoConfiguration.ts
@@ -1,8 +1,9 @@
 import { getBaseClientAutoConfiguration } from "../../core/BaseClientAutoConfiguration";
+import { getTokenClientAutoConfiguration } from "../token";
 import { RandomClientConfig } from "./abstract/RandomClientConfig";
 
 export const getRandomClientAutoConfiguration = (): RandomClientConfig => ({
     ...getBaseClientAutoConfiguration(),
     processId: "KbaY8P4h9wdHYKHlBSLbXN_yd-9gxUDxSgBackUxTiQ",
-    tokenProcessId: "7enZBOhWsyU3A5oCt8HtMNNPHSxXYJVTlOGOetR9IDw",
+    tokenProcessId: getTokenClientAutoConfiguration().processId,
 });
