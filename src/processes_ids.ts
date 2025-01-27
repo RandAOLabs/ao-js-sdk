fb74b458e2287771a18022a472fe832f76b28fe8
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-01-27 16:36:33 -0500

    collection might work

--- src/processes_ids.ts
+++ src/processes_ids.ts
@@ -1,2 +1,3 @@
 export const RNG_TOKEN_PROCESS_ID: string = "5ZR9uegKoEhE9fJMbs-MvWLIztMNCVxgpzfeBVE3vqI"
 export const RANDOM_PROCESS_ID: string = "KbaY8P4h9wdHYKHlBSLbXN_yd-9gxUDxSgBackUxTiQ"
+export const COLLECTION_PROCESS_ID: string = "COLLECTION_PROCESS_ID_PLACEHOLDER" // TODO: Replace with actual process ID
