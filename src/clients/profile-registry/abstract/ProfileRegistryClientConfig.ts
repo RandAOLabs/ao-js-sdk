51ebec8923404d1f2aa6a35129cb3d9a030dd17c
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-04 10:14:59 -0500

    profiles and registry (#39)

---
+++ src/clients/profile-registry/abstract/ProfileRegistryClientConfig.ts
@@ -0,0 +1,5 @@
+import { BaseClientConfig } from "../../../core/abstract/BaseClientConfig";
+
+export interface ProfileRegistryClientConfig extends BaseClientConfig {
+    // Registry Specific Configuration Items
+}
