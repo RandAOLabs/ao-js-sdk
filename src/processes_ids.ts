13cfdf18979ce33290afd4caacf07c79c86fce0c
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-07 17:50:37 -0500

    colleciton

--- src/processes_ids.ts
+++ src/processes_ids.ts
@@ -4,9 +4,9 @@ export const RANDOM_PROCESS_ID: string = "KbaY8P4h9wdHYKHlBSLbXN_yd-9gxUDxSgBack
 export const STAKING_PROCESS_ID: string = RNG_TOKEN_PROCESS_ID
 export const STAKING_TOKEN_PROCESS_ID: string = "5ZR9uegKoEhE9fJMbs-MvWLIztMNCVxgpzfeBVE3vqI"
 
-export const NFT_SALE_PROCESS_ID: string = "VTJPsVvqXc9KSPn-XNfX8sQgWk1EDXU7zijSdTwbqp0"
+export const NFT_SALE_PROCESS_ID: string = "y__FX6IgcXDOlOIEZqWZDU1k1dxYSpBICf-wxv25Tj0"
 export const ARCAO_TEST_TOKEN_PROCESS_ID: string = STAKING_TOKEN_PROCESS_ID
-export const ARCAO_TEST_NFT_COLLECTION: string = "v7FHEOahrCsY82bo93_vGHHeO13KwuME-EA72ZXHWwM"
+export const ARCAO_TEST_NFT_COLLECTION: string = "Kvrlp7PFbtxLohR8g5PKYRzKccAsK09cpAWqGLLL_1M"
 
 /*In theory these never need to change*/
 export const PROFILE_REGISTRY_PROCESS_ID: string = "SNy4m-DrqxWl01YqGM4sxI8qCni-58re8uuJLvZPypY"
