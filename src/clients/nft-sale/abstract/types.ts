0be9233628e658ba8d89cdaeee6d149b1bfa5184
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-08 06:15:39 -0500

    update return type

--- src/clients/nft-sale/abstract/types.ts
+++ src/clients/nft-sale/abstract/types.ts
@@ -1,3 +1,18 @@
 import { Tags } from "../../../core/abstract/types";
 
-export type NftSaleInfo = Record<string, any>;
+export interface WhitelistZone {
+    [1]: string;  // Zone Purchase Price
+    [2]: string;  // Zone Lucky Price
+    [3]: {
+        [key: string]: boolean;  // Whitelist addresses mapping
+    };
+}
+
+export interface WhitelistMaster {
+    [key: number]: WhitelistZone;  // Zone number mapping
+}
+
+export interface NftSaleInfo {
+    Current_Zone: number;
+    MasterWhitelist: WhitelistMaster;
+}
