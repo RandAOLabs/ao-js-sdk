e99957ed2c313fcc88066dfba929f14a9e6cbb9c
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-08 09:52:42 -0500

    profile auto

--- src/clients/nft-sale/NftSaleClientError.ts
+++ src/clients/nft-sale/NftSaleClientError.ts
@@ -1,36 +1,48 @@
 import { BaseClientError } from "../../core";
 
-export class PurchaseNftError extends BaseClientError {
+export class NftSaleClientError extends BaseClientError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+    }
+}
+
+export class PaymentError extends NftSaleClientError {
+    constructor(amount: string, cause?: Error) {
+        super(`Failed to process payment of ${amount} tokens`, cause);
+    }
+}
+
+export class PurchaseNftError extends NftSaleClientError {
     constructor(amount: string, cause?: Error) {
         super(`Failed to purchase NFT for ${amount} tokens`, cause);
     }
 }
 
-export class QueryNFTCountError extends BaseClientError {
+export class QueryNFTCountError extends NftSaleClientError {
     constructor(cause?: Error) {
         super("Failed to query NFT count", cause);
     }
 }
 
-export class AddNftError extends BaseClientError {
+export class AddNftError extends NftSaleClientError {
     constructor(nftProcessId: string, cause?: Error) {
         super(`Failed to add NFT from process ${nftProcessId}`, cause);
     }
 }
 
-export class ReturnNFTsError extends BaseClientError {
+export class ReturnNFTsError extends NftSaleClientError {
     constructor(recipient: string, cause?: Error) {
         super(`Failed to return NFTs to recipient ${recipient}`, cause);
     }
 }
 
-export class LuckyDrawError extends BaseClientError {
+export class LuckyDrawError extends NftSaleClientError {
     constructor(amount: string, cause?: Error) {
         super(`Failed to participate in lucky draw for ${amount} tokens`, cause);
     }
 }
 
-export class NftSaleInfoError extends BaseClientError {
+export class NftSaleInfoError extends NftSaleClientError {
     constructor(cause?: Error) {
         super("Failed to get NFT sale information", cause);
     }
