9e599e56ccf96a528aca279d73398cffaa13f15c
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-08 14:46:43 -0500

    payments

--- src/clients/nft-sale/NftSaleClientAutoConfiguration.ts
+++ src/clients/nft-sale/NftSaleClientAutoConfiguration.ts
@@ -2,13 +2,8 @@ import { getBaseClientAutoConfiguration } from "../../core/ao/BaseClientAutoConf
 import { NftSaleClientConfig } from "./abstract/NftSaleClientConfig";
 import { NFT_SALE_PROCESS_ID, ARCAO_TEST_TOKEN_PROCESS_ID } from "../../processes_ids";
 
-const DEFAULT_PURCHASE_AMOUNT = "1000000"; // 1 token with 18 decimals
-const DEFAULT_LUCKY_DRAW_AMOUNT = "1000000"; // 1 token with 18 decimals
-
 export const getNftSaleClientAutoConfiguration = (): NftSaleClientConfig => ({
     ...getBaseClientAutoConfiguration(),
     processId: NFT_SALE_PROCESS_ID,
     tokenProcessId: ARCAO_TEST_TOKEN_PROCESS_ID,
-    purchaseAmount: DEFAULT_PURCHASE_AMOUNT,
-    luckyDrawAmount: DEFAULT_LUCKY_DRAW_AMOUNT,
 });
