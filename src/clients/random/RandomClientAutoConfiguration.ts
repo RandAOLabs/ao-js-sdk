4f7ae24c4c84b7d2779014b1e60cb86ee8361ac1
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-12 15:02:06 -0500

    domains

--- src/clients/random/RandomClientAutoConfiguration.ts
+++ src/clients/random/RandomClientAutoConfiguration.ts
@@ -1,11 +1,11 @@
 import { RandomClientConfig } from "src/clients/random/abstract";
-import { RANDAO_API_ARNS_NAME } from "src/clients/random/constants";
 import { getBaseClientAutoConfiguration } from "src/core/ao/BaseClientAutoConfiguration";
 import { RNG_TOKEN_PROCESS_ID } from "src/processes_ids"
 import ARIOService from "src/services/ario/ARIOService";
+import { DOMAINS } from "src/services/ario/domains";
 
 export const getRandomClientAutoConfiguration = async (): Promise<RandomClientConfig> => ({
     ...getBaseClientAutoConfiguration(),
-    processId: await ARIOService.getProcessIdForDomain(RANDAO_API_ARNS_NAME),
+    processId: await ARIOService.getProcessIdForDomain(DOMAINS.RANDAO_API),
     tokenProcessId: RNG_TOKEN_PROCESS_ID,
 });
