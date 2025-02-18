89ac41fab0f9c6313dc2fc438499f24d04e0fb1e
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-17 20:13:22 -0500

    afasdf

--- src/services/profiles/index.ts
+++ src/services/profiles/index.ts
@@ -2,8 +2,6 @@
 export * from './abstract/IProfilesService';
 export * from './ProfilesService';
 
-// Export errors
-export * from './ProfilesError';
 
 // Export default singleton instance
 export { default } from './ProfilesService';
