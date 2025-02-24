b2e98943f336380840b11e0d792a3c7a71333c69
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-24 10:54:14 -0500

    eee (#83)

--- src/utils/wallet/environmentWallet.ts
+++ src/utils/wallet/environmentWallet.ts
@@ -40,7 +40,7 @@ export function getWallet(): JWKInterface | undefined {
                     Logger.warn(`Warning: Missing environment variable: ${error.message}`)
                     return undefined;
                 } else if (error instanceof Error) {
-                    throw new FileReadError(pathToWallet, error.message);
+                    Logger.warn(`Error while getting environment wallet: ${error.message}`)
                 } else {
                     throw new FileReadError(pathToWallet, 'Unknown error');
                 }
