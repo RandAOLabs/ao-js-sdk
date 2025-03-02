dbde58c8b9850d54c013dba6ee93470c30da4814
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-02 15:58:29 -0500

    Arweave gql in browser (#89)

---
+++ src/core/arweave/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Default Arweave connection configuration used across environments
+ */
+export const ARWEAVE_CONFIG = {
+    host: 'arweave-search.goldsky.com',
+    port: 443,
+    protocol: 'https'
+} as const;
