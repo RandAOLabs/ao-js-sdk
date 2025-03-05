cfb236c3020dc8795f24495af5cbc2522c47be04
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-05 11:04:53 -0500

    done

--- src/clients/ao/token/Tokens.ts
+++ src/clients/ao/token/Tokens.ts
@@ -20,16 +20,36 @@ const RuneRealmTokens = {
 /**
  * Number Always Bigger process ID
  */
-const NUMBER_ALWAYS_BIGGER = "OsK9Vgjxo0ypX_HLz2iJJuh4hp3I80yA9KArsJjIloU";
+const NUMBER_ALWAYS_BIGGER: string = "OsK9Vgjxo0ypX_HLz2iJJuh4hp3I80yA9KArsJjIloU";
 
 /**
  * TRUNK process ID
  */
-const TRUNK = "wOrb8b_V8QixWyXZub48Ki5B6OIDyf_p1ngoonsaRpQ";
+const TRUNK: string = "wOrb8b_V8QixWyXZub48Ki5B6OIDyf_p1ngoonsaRpQ";
+
+/**
+ * Wrapped AR (wAR) process ID
+ */
+const WRAPPED_ARWEAVE: string = "xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10";
+
+/**
+ * Quantom AR (qAR) process ID
+ */
+const QUANTOM_ARWEAVE: string = "NG-0lVX882MG5nhARrSzyprEK6ejonHpdUmaaMPsHE8"
+
+/**
+ * AO Token process ID
+ */
+const AO: string = "UkS-mdoiG8hcAClhKK8ch4ZhEzla0mCPDOix9hpdSFE"
+
+
 
 // Export all tokens in a single namespace
 export const TOKENS = {
     RUNE_REALM: RuneRealmTokens,
     NUMBER_ALWAYS_BIGGER,
+    WRAPPED_ARWEAVE,
+    QUANTOM_ARWEAVE,
+    AO,
     TRUNK
 } as const;
