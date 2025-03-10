cc38157b4f6d47faefc918bfb9d528f1b0ade8c2
Ethan <Ethan.Gold-Steinberg@protonmail.com>
2025-03-10 10:54:43 -0400

    Update to interface

--- src/services/randao/abstract/IRandAOService.ts
+++ src/services/randao/abstract/IRandAOService.ts
@@ -2,5 +2,27 @@
  * Interface for the RandAO Service
  */
 export interface IRandAOService {
+    /**
+     * Retrieves All info on the random providers
+     * @returns An array of {ProviderInfo,ProviderActivity,GraphQlData} linked by address
+     */
+    getAllProviderInfo(): Promise<any[]>;
 
 }
+
+
+
+// const queryObject = {
+//     query: `{
+//       transactions(
+//         recipients: ["${RANDOMPROCCESS}"],
+//         owners: ["${provider_id}"],
+//         tags: [
+//           { name: "Action", values: ["Post-VDF-Output-And-Proof"] },
+//           { name: "Data-Protocol", values: ["ao"] }
+//         ]
+//       ) {
+//          count
+//       }
+//     }`
+//   };
