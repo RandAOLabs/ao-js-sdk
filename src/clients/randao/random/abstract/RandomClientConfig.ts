178175ff3f94c296601c310365c560dfcc55c8ef
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-17 16:21:36 -0400

    lots of retries

--- src/clients/randao/random/abstract/RandomClientConfig.ts
+++ src/clients/randao/random/abstract/RandomClientConfig.ts
@@ -1,5 +1,5 @@
+import { TokenInterfacingClientConfig } from "src/clients/common/TokenInterfacingClientConfig";
 import { BaseClientConfig } from "src/index";
 
-export interface RandomClientConfig extends BaseClientConfig {
-    tokenProcessId: string
+export interface RandomClientConfig extends TokenInterfacingClientConfig {
 }
