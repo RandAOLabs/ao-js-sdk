eb89a0e9df0fdacf2775aed03cc0d417b193068a
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-17 18:14:39 -0400

    esz

--- src/utils/class-interfaces/IDefaultBuilder.ts
+++ src/utils/class-interfaces/IDefaultBuilder.ts
@@ -1,12 +1,11 @@
-import { BaseClient } from "src/core/ao/BaseClient";
 import { IBuilder } from "./IBuilder";
 import { UnimplementedError } from "../errors";
 
-export abstract class IDefaultBuilder{
+export abstract class IDefaultBuilder {
     /**
      * @returns a builder with nice preset defaults
      */
     public static defaultBuilder<T extends IDefaultBuilder>(): IBuilder<T> {
-            throw new UnimplementedError('defaultBuilder', this.name)
-        }
+        throw new UnimplementedError('defaultBuilder', this.name)
+    }
 }
