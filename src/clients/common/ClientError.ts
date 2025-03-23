4a81c2aec5bbae641d0717f1c11b3a3861f8779e
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-22 20:06:39 -0400

    token client error handeling improvoved

---
+++ src/clients/common/ClientError.ts
@@ -0,0 +1,27 @@
+import { IBaseClient } from "src/core/ao/abstract";
+import { Logger } from "src/utils";
+
+export class ClientError<T extends IBaseClient, P = any> extends Error {
+    public client: T
+    constructor(client: T, func: Function, params: P, originalError?: Error) {
+        const functionName = func.name;
+        const paramsString = JSON.stringify(params, null, 2);
+
+        const fullMessage: string = `
+            \n| Error in ${typeof client} |
+            \n| Occurred while executing function: ${functionName} |
+            \n| With parameters: ${paramsString} |
+            \n| Process Id: ${client.getProcessId()} |
+            \n| ReadOnly: ${client.isReadOnly()} |
+            \n| ${originalError ? `Error was caused by: ${originalError.message}` : `Cause not specified`} |
+        `
+        super(fullMessage);
+        this.client = client
+        this.name = `${typeof client} Error`;
+        Logger.error(fullMessage)
+
+        if (originalError) {
+            this.stack += '\nCaused by: ' + originalError.stack;
+        }
+    }
+}
