69b7ab42bbb6aa5da6ea1dfb4dc49a3167aeacf5
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-22 20:06:39 -0400

    token client error handeling improvoved

--- src/clients/ao/token/abstract/types.ts
+++ src/clients/ao/token/abstract/types.ts
@@ -1,7 +1,37 @@
+import { Tags } from "src/core";
+
 export type BalanceResponse = {
     Tags: {
         Balance: string;
         Target: string;
         Ticker: string;
     };
+};
+
+export type BalanceParams = {
+    identifier?: string;
+};
+
+export type BalancesParams = {
+    limit?: number;
+    cursor?: string;
+};
+
+export type TransferParams = {
+    recipient: string;
+    quantity: string;
+    forwardedTags?: Tags;
+};
+
+export type GetInfoParams = {
+    token: string;
+};
+
+export type MintParams = {
+    quantity: string;
+};
+
+export type GrantParams = {
+    quantity: string;
+    recipient?: string;
 };
