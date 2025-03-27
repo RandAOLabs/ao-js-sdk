5df728bb35c97ba08fb0cac99d649d6c2e5fd2fb
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-27 11:38:30 -0400

    okay

--- src/core/common/result-utils/ResultUtilsError.ts
+++ src/core/common/result-utils/ResultUtilsError.ts
@@ -1,24 +1,41 @@
-// BaseClientError.ts
-export class ResultUtilsError extends Error {
-    constructor(message: string, originalError?: Error) {
+import { DryRunResult } from "@permaweb/aoconnect/dist/lib/dryrun";
+import { MessageResult } from "@permaweb/aoconnect/dist/lib/result";
+import { Logger } from "src/utils";
+
+export class ResultReadingError extends Error {
+
+    constructor(result: MessageResult | DryRunResult, detailMessage?: string, originalError?: Error) {
+        const message = `
+            | Error Reading ${typeof result}: ${JSON.stringify(result)} |
+            | ${detailMessage ? detailMessage : "Unknown Error"} |
+            | Caused by: ${originalError ? JSON.stringify(originalError) : "No upstream error"}|
+        `
         super(message);
-        this.name = 'ResultUtilsError';
+        this.name = 'ResultReadingError';
         if (originalError) {
             this.stack += '\nCaused by: ' + originalError.stack;
         }
+        Logger.error(message)
     }
 }
 
-export class JsonParsingError extends ResultUtilsError {
-    constructor(message: string, originalError?: Error) {
-        super(`Error parsing JSON data: ${message}`, originalError);
+export class JsonParsingError extends ResultReadingError {
+    constructor(result: MessageResult | DryRunResult, index: number, originalError?: Error) {
+        super(result, `Error parsing JSON data at index ${index}: ${result.Messages[index]?.Data}`, originalError);
         this.name = 'JsonParsingError';
     }
 }
 
-export class MessageOutOfBoundsError extends ResultUtilsError {
-    constructor(index: number, length: number) {
-        super(`Index out of bounds: ${index}. Total messages available: ${length}`);
+export class MessageOutOfBoundsError extends ResultReadingError {
+    constructor(result: MessageResult | DryRunResult, index: number) {
+        super(result, `Index out of bounds: ${index}.Total messages available: ${result.Messages.length}`)
         this.name = 'MessageOutOfBoundsError';
+    }
+}
+
+export class MessagesMissingError extends ResultReadingError {
+    constructor(result: MessageResult | DryRunResult) {
+        super(result, "Result contains no messages, however messages were expected.");
+        this.name = 'MessagesMissingError';
     }
 }
