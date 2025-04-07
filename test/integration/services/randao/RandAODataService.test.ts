59a4b094fcf42d32f44c8c81aadf617a08552e08
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-07 17:07:10 -0400

    int test not working

---
+++ test/integration/services/randao/RandAODataService.test.ts
@@ -0,0 +1,25 @@
+import { Logger, LogLevel } from "src/utils";
+import { RandAODataService } from "../../../../src/services/randao/RandAODataService";
+import { IRandAODataService } from "src/services";
+
+jest.setTimeout(6000000);
+describe("RandAODataService Integration Tests", () => {
+	let service: IRandAODataService;
+
+	beforeAll(async () => {
+		Logger.setLogLevel(LogLevel.DEBUG);
+		service = await RandAODataService.autoConfiguration();
+	});
+
+	describe("getTotalRandomResponses", () => {
+		it("should return total count of random responses", async () => {
+			Logger.info("----------------------");
+			const result = await service.getTotalRandomResponses();
+			Logger.info(`Total random responses: ${result}`);
+			Logger.info("----------------------");
+
+			expect(typeof result).toBe("number");
+			expect(result).toBeGreaterThanOrEqual(0);
+		});
+	});
+});
