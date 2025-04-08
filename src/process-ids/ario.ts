42015a02d982a31ddc9961c2a80e4b260d2533a0
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-08 05:58:59 -0400

    comment

--- src/process-ids/ario.ts
+++ src/process-ids/ario.ts
@@ -1,3 +1,5 @@
+/*https://github.com/ar-io/ar-io-sdk/blob/7cfc23cfe00dfbeacd8e1c125626fe54693e73d4/src/constants.ts#L40 */
+
 export const ARIO_ARNS_REGISTRY_PROCESS_ID: string = "qNvAoz0TgcH7DMg8BCVn8jF32QH5L6T29VjHxhHqqGE"
 
 export const ARIO = {
