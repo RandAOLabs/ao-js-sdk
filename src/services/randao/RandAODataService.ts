001ab724136be4a52db19d3d45bfd5325eb0b685
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-08 06:21:45 -0400

    sdsd

--- src/services/randao/RandAODataService.ts
+++ src/services/randao/RandAODataService.ts
@@ -1,10 +1,11 @@
-import { IAutoconfiguration } from "../../utils";
+import { IAutoconfiguration, Logger } from "../../utils";
 import { staticImplements } from "../../utils/decorators";
 import { ARIOService, IARIOService } from "../ario";
 import { DOMAIN } from "../ario/domains";
 import { IMessagesService, MessagesService } from "../messages";
 import { IRandAODataService } from "./abstract/IRandAODataService";
 import RANDOM_PROCESS_TAGS from "../../clients/randao/random/tags";
+import { SYSTEM_TAGS } from "../../core/common/tags";
 
 
 /**
@@ -31,9 +32,12 @@ export class RandAODataService implements IRandAODataService {
 
 	async getTotalRandomResponses(): Promise<number> {
 		const randomProcessId = await this.arioservice.getProcessIdForDomain(DOMAIN.RANDAO_API)
+		Logger.debug(randomProcessId)
 		return await this.messagesService.countAllMessages({
-			owner: randomProcessId,
-			tags: [RANDOM_PROCESS_TAGS.ACTION.RESPONSE]
+			tags: [
+				RANDOM_PROCESS_TAGS.ACTION.RESPONSE,
+				SYSTEM_TAGS.FROM_PROCESS(randomProcessId)
+			]
 		})
 	}
 
