8371d757311c37b14e5ac369d4813603cc96c04a
Ethan-GoldS <Ethan.gold-Steinberg@protonmail.com>
2025-04-09 11:58:10 -0400

    done (#141)

--- src/core/ao/ao-client/configurations.ts
+++ src/core/ao/ao-client/configurations.ts
@@ -1,6 +1,6 @@
 import { ConnectArgsLegacy } from "./aoconnect-types";
 
-export const FORWARD_RESEARCH_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ao-testnet.xyz', MODE: 'legacy' }
+export const FORWARD_RESEARCH_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.randao.net', MU_URL:'https://mu.randao.net',MODE: 'legacy' }
 export const ARDRIVE_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ardrive.io', MODE: 'legacy' }
 export const ARIO_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ar-io.dev', MODE: 'legacy' }
 export const RANDAO_AO_CONFIG: ConnectArgsLegacy = { CU_URL: ' https://cu.randao.net', MU_URL:'https://mu.randao.net',MODE: 'legacy' }
