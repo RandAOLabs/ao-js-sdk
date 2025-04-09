053f18ebb5ceae214ec812027cdf83e23dbd343e
Ethan-GoldS <Ethan.gold-Steinberg@protonmail.com>
2025-04-09 10:16:35 -0400

    Done (#137)

--- src/process-ids/randao.ts
+++ src/process-ids/randao.ts
@@ -1,5 +1,5 @@
-const RANDAO_STAKING_PROCESS_ID: string = "IzAxA1LtyECx4WJXmiReaeizvuyfXzikvrQHtv-ZNrI"
-const RANDAO_RNG_TOKEN_PROCESS_ID: string = "saDyEF8Ddg7mUuhBOCrHpfsjejPoTVDKogQBxWQ-DR4"
+const RANDAO_STAKING_PROCESS_ID: string = "GZN3ouFj7db4qzpmCCTo6k3sCpZWoYQp-RoPGI-Cxuk"
+const RANDAO_RNG_TOKEN_PROCESS_ID: string = "MlcQtVXNE34KhWoYgIDAwvQ1g9nhQvaYHTtsJVvUJfg"
 
 
 
@@ -10,7 +10,7 @@ export const RANDAO = {
      * Typically retrieved from chain dynamically this value may be stale
      * arns name: api_randao
      */
-    RANDOM: "FOarjI5mCtPUUYZ2pRTTNX0erBik0O1g-0I1R4WTl-U",
+    RANDOM: "9ZXTvznyUo266uIU8nHPMsAtI8NntFdmsET3M3Hv_FA",
     STAKING: RANDAO_STAKING_PROCESS_ID,
     PROFILE: RANDAO_STAKING_PROCESS_ID,
 }
