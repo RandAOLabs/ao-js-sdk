3fef17d25abe1f5240aa7f068330e4967673b0ec
Ethan-GoldS <Ethan.gold-Steinberg@protonmail.com>
2025-04-10 17:11:16 -0400

    done (#142)

--- src/core/ao/ao-client/configurations.ts
+++ src/core/ao/ao-client/configurations.ts
@@ -1,9 +1,9 @@
 import { ConnectArgsLegacy } from "./aoconnect-types";
 
-export const FORWARD_RESEARCH_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.randao.net', MU_URL:'https://mu.randao.net',MODE: 'legacy' }
+export const FORWARD_RESEARCH_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ao-testnet.xyz', MU_URL:'https://mu.ao-testnet.xyz', MODE: 'legacy' }
 export const ARDRIVE_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ardrive.io', MODE: 'legacy' }
 export const ARIO_AO_CONFIG: ConnectArgsLegacy = { CU_URL: 'https://cu.ar-io.dev', MODE: 'legacy' }
-export const RANDAO_AO_CONFIG: ConnectArgsLegacy = { CU_URL: ' https://cu.randao.net', MU_URL:'https://mu.randao.net',MODE: 'legacy' }
+export const RANDAO_AO_CONFIG: ConnectArgsLegacy = { CU_URL: ' https://ur-cu.randao.net', MU_URL:'https://ur-mu.randao.net',MODE: 'legacy' }
 
 export const AO_CONFIGURATIONS = {
     FORWARD_RESEARCH: FORWARD_RESEARCH_AO_CONFIG,
@@ -12,4 +12,4 @@ export const AO_CONFIGURATIONS = {
     RANDAO: RANDAO_AO_CONFIG,
 }
 
-export const AO_CONFIGURATION_DEFAULT: ConnectArgsLegacy = FORWARD_RESEARCH_AO_CONFIG;
+export const AO_CONFIGURATION_DEFAULT: ConnectArgsLegacy = RANDAO_AO_CONFIG;
