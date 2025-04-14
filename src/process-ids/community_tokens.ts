ca5eee5c035dc38c0698d348e8bf2bd8c4e064f3
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-14 12:06:06 -0400

    SEND IT (#145)

--- src/process-ids/community_tokens.ts
+++ src/process-ids/community_tokens.ts
@@ -1,6 +1,7 @@
 
 export const COMMUNITY_TOKENS = {
-    TRUNK: "wOrb8b_V8QixWyXZub48Ki5B6OIDyf_p1ngoonsaRpQ",
-    NUMBER_ALWAYS_BIGGER: "OsK9Vgjxo0ypX_HLz2iJJuh4hp3I80yA9KArsJjIloU"
+	TRUNK: "wOrb8b_V8QixWyXZub48Ki5B6OIDyf_p1ngoonsaRpQ",
+	NUMBER_ALWAYS_BIGGER: "OsK9Vgjxo0ypX_HLz2iJJuh4hp3I80yA9KArsJjIloU",
+	SEND: "O2mmX6O7ZNUnRfE8pYtzTT2uH55dXrvIR8YeaOEsp9I"
 }
 
