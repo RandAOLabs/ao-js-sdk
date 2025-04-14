8831f1dc2e1967cccae51f15eb372e96fa943d34
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-14 12:04:50 -0400

    SEND IT

---
+++ src/clients/tokens/SendTokenClient.ts
@@ -0,0 +1,30 @@
+import { TokenClient } from "../ao";
+import { ClientBuilder } from "../common";
+import { PROCESS_IDS } from "../../process-ids";
+import { staticImplements, IAutoconfiguration, IDefaultBuilder } from "../../utils";
+
+/**
+ * Client for interacting with SEND token.
+ * @category Tokens
+ */
+@staticImplements<IAutoconfiguration>()
+@staticImplements<IDefaultBuilder>()
+export class SendToken extends TokenClient {
+	/** 
+	 * {@inheritdoc IAutoconfiguration.autoConfiguration}
+	 * @see {@link IAutoconfiguration.autoConfiguration} 
+	 */
+	public static autoConfiguration(): SendToken {
+		return SendToken.defaultBuilder()
+			.build()
+	}
+
+	/** 
+	 * {@inheritdoc IDefaultBuilder.defaultBuilder}
+	 * @see {@link IDefaultBuilder.defaultBuilder} 
+	 */
+	public static defaultBuilder(): ClientBuilder<SendToken> {
+		return new ClientBuilder(SendToken)
+			.withProcessId(PROCESS_IDS.COMMUNITY_TOKENS.SEND)
+	}
+}
