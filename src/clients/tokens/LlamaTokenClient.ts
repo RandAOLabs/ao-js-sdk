07840bdb311458859ee65069dee9d5f8db423e66
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-28 13:39:00 -0400

    new tokens?

---
+++ src/clients/tokens/LlamaTokenClient.ts
@@ -0,0 +1,27 @@
+import { TokenClient } from "../ao";
+import { ClientBuilder } from "../common";
+import { COMMUNITY_TOKENS } from "../../process-ids/community_tokens";
+import { staticImplements, IAutoconfiguration, IDefaultBuilder } from "../../utils";
+/**
+ * @category Tokens
+ */
+@staticImplements<IAutoconfiguration>()
+@staticImplements<IDefaultBuilder>()
+export class LlamaToken extends TokenClient {
+	/** 
+	 * {@inheritdoc IAutoconfiguration.autoConfiguration}
+	 * @see {@link IAutoconfiguration.autoConfiguration} 
+	 */
+	public static autoConfiguration(): LlamaToken {
+		return LlamaToken.defaultBuilder()
+			.build()
+	}
+	/** 
+	 * {@inheritdoc IDefaultBuilder.defaultBuilder}
+	 * @see {@link IDefaultBuilder.defaultBuilder} 
+	 */
+	public static defaultBuilder(): ClientBuilder<LlamaToken> {
+		return new ClientBuilder(LlamaToken)
+			.withProcessId(COMMUNITY_TOKENS.LLAMA)
+	}
+}
