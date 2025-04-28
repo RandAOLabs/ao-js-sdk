dd861ed8c9a78f3b9037a0c124e2c9ef9e990a50
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-04-28 13:40:27 -0400

    new tokens? (#153)

---
+++ src/clients/tokens/DumDumTokenClient.ts
@@ -0,0 +1,27 @@
+import { TokenClient } from "../ao";
+import { ClientBuilder } from "../common";
+import { COMMUNITY_TOKENS } from "../../process-ids/community_tokens";
+import { staticImplements, IAutoconfiguration, IDefaultBuilder } from "../../utils";
+/**
+ * @category Tokens
+ */
+@staticImplements<IAutoconfiguration>()
+@staticImplements<IDefaultBuilder>()
+export class DumDumToken extends TokenClient {
+	/** 
+	 * {@inheritdoc IAutoconfiguration.autoConfiguration}
+	 * @see {@link IAutoconfiguration.autoConfiguration} 
+	 */
+	public static autoConfiguration(): DumDumToken {
+		return DumDumToken.defaultBuilder()
+			.build()
+	}
+	/** 
+	 * {@inheritdoc IDefaultBuilder.defaultBuilder}
+	 * @see {@link IDefaultBuilder.defaultBuilder} 
+	 */
+	public static defaultBuilder(): ClientBuilder<DumDumToken> {
+		return new ClientBuilder(DumDumToken)
+			.withProcessId(COMMUNITY_TOKENS.DUMDUM)
+	}
+}
