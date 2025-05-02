60bc9f2317acdbb1e5c00e9b2c8be0415e9339a1
Ethan-GoldS <Ethan.gold-Steinberg@protonmail.com>
2025-05-02 07:56:42 -0400

    Added pi token (#159)

---
+++ src/clients/pi/abstract/index.ts
@@ -0,0 +1,2 @@
+export * from './IPIClient';
+export * from './types';
---
+++ src/clients/pi/constants.ts
@@ -0,0 +1,24 @@
+/**
+ * Process IDs for PI-related processes
+ */
+export const PI_TOKEN_PROCESS_ID = 'UcBPqkaVI7W4I_YMznrt2JUoyc_7TScCdZWOOSBvMSU';
+export const PI_DELEGATE_PROCESS_ID = 'cuxSKjGJ-WDB9PzSkVkVVrIBSh3DrYHYz44usQOj5yE';
+export const DELEGATION_ORACLE_PROCESS_ID = 'It-_AKlEfARBmJdbJew1nG9_hIaZt0t20wQc28mFGBE';
+
+/**
+ * Action tags for PI processes
+ */
+export const ACTION_INFO = 'Info';
+export const ACTION_GET_DELEGATIONS = 'Get-Delegations';
+export const ACTION_GET_YIELD_TICK_HISTORY = 'Get-Yield-Tick-History';
+export const ACTION_GET_FLPS = 'Get-FLPs';
+export const ACTION_BALANCE = 'Balance';
+export const ACTION_GET_CLAIMABLE_BALANCE = 'Get-Claimable-Balance';
+
+/**
+ * Response messages for PI processes
+ */
+export const DELEGATION_INFO_SUCCESS_MESSAGE = 'Get-Delegations';
+export const TOKEN_INFO_SUCCESS_MESSAGE = 'Info-Response';
+export const TICK_HISTORY_SUCCESS_MESSAGE = 'Get-Yield-Tick-History';
+export const PI_TOKENS_SUCCESS_MESSAGE = 'Get-FLPs';
