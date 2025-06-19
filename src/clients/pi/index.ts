6bfc655b87d3eecc2547b95a44c5541f87c9fe57
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-06-19 17:44:38 -0400

    FLPS (#203)

--- src/clients/pi/index.ts
+++ src/clients/pi/index.ts
@@ -2,12 +2,13 @@
  * PI Client Module
  * 
  * This module provides clients for interacting with PI-related processes on the AO network.
- * The module consists of four main clients:
+ * The module consists of five main clients:
  * 
  * 1. PIOracleClient - For fetching information about available PI tokens
  * 2. PITokenClient - For interacting with specific PI token processes
  * 3. PIDelegateClient - For delegation-related functionality
  * 4. DelegationHistorianClient - For fetching and parsing delegation history records
+ * 5. FairLaunchProcessClient - For interacting with fair launch processes
  */
 
 // Export all client modules
@@ -15,7 +16,7 @@ export * from './PIToken'
 export * from './oracle'
 export * from './delegate'
 export * from './historian'
+export * from './fair-launch-process'
 
 // Export constants
 export * from './constants'
-
