e6b2b0d1567685d0ba658d39e0f799973cf17626
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-06-19 20:11:48 -0400

    gamer token

--- src/clients/tokens/GameTokenClient.ts
+++ src/clients/tokens/GameTokenClient.ts
@@ -1,11 +1,12 @@
 import { AO_CONFIGURATION_DEFAULT } from "../../core/ao/ao-client/configurations";
 import { ARCAO } from "../../process-ids/arcao";
 import { TokenClient } from "../ao";
+import { ClientBuilder } from "../common";
 
 /**
  * Pre-configured GAME tokenClient
  */
-export const GameToken  = TokenClient.builder()
+export const GameToken  = new ClientBuilder(TokenClient)
 			.withAOConfig(AO_CONFIGURATION_DEFAULT)
 			.withProcessId(ARCAO.GAME_TOKEN)
 			.build();
