336186a7b1839fc246c5c9648c1568f92b572139
Allan <31974198+BrewDogDev@users.noreply.github.com>
2025-07-23 15:17:58 -0400

    ARNS data wrangled

--- src/services/ario/arns-data-service/ARNSDataService.ts
+++ src/services/ario/arns-data-service/ARNSDataService.ts
@@ -1,5 +1,88 @@
+import { Observable } from "rxjs";
+import { ArweaveTransaction } from "../../../core/arweave/abstract/types";
 import { IARNSDataService } from "./abstract";
+import { staticImplements, IAutoconfiguration } from "../../../utils";
+import { IReactiveMessageService, ReactiveMessageService } from "../../messages";
+import { ARNS_TAGS } from "../shared";
 
+
+@staticImplements<IAutoconfiguration>()
 export class ARNSDataService implements IARNSDataService {
+	constructor(
+		private readonly reactiveMessageService: IReactiveMessageService,
+	) { }
+
+	/**
+	 * Creates a pre-configured instance of PiDataService
+	 * @returns A pre-configured PiDataService instance
+	 */
+	public static autoConfiguration(): IARNSDataService {
+		return new ARNSDataService(
+			ReactiveMessageService.autoConfiguration()
+		);
+	}
+
+	getBuyNameNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.BUYNAME_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getRecordNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.RECORD_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getUpgradeNameNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.UPGRADENAME_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getExtendLeaseNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.EXTENDLEASE_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getIncreaseUndernameNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.INCREASEUNDERNAMELIMIT_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getReassignNameNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.REASSIGNNAME_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
+
+	getReturnedNameNotices(name: string): Observable<ArweaveTransaction[]> {
+		return this.reactiveMessageService.streamAllMessages({
+			tags: [
+				ARNS_TAGS.ACTION.RETURNEDNAME_NOTICE,
+				ARNS_TAGS.ACTION.NAME(name)
+			]
+		})
+	}
 
 }
---
+++ src/services/ario/shared/tags.ts
@@ -0,0 +1,117 @@
+import { Tag } from "../../../core/common/types";
+
+const ACTION_TAG_NAME = "Action"
+const NAME_TAG_NAME = "Name"
+
+// Type helper to ensure all values are Tags or Tag-generating functions while maintaining IntelliSense
+type TagRecord<T extends Record<string, Tag | ((value: string) => Tag)>> = T;
+
+export const ARNS_TAGS = {
+	ACTION: {
+		RECORD: {
+			name: ACTION_TAG_NAME,
+			value: "Record"
+		},
+		RECORDS: {
+			name: ACTION_TAG_NAME,
+			value: "Records"
+		},
+		BUYNAME: {
+			name: ACTION_TAG_NAME,
+			value: "Buy-Name"
+		},
+		UPGRADENAME: {
+			name: ACTION_TAG_NAME,
+			value: "Upgrade-Name"
+		},
+		EXTENDLEASE: {
+			name: ACTION_TAG_NAME,
+			value: "Extend-Lease"
+		},
+		INCREASEUNDERNAMELIMIT: {
+			name: ACTION_TAG_NAME,
+			value: "Increase-Undername-Limit"
+		},
+		REASSIGNNAME: {
+			name: ACTION_TAG_NAME,
+			value: "Reassign-Name"
+		},
+		RELEASENAME: {
+			name: ACTION_TAG_NAME,
+			value: "Release-Name"
+		},
+		RESERVEDNAMES: {
+			name: ACTION_TAG_NAME,
+			value: "Reserved-Names"
+		},
+		RESERVEDNAME: {
+			name: ACTION_TAG_NAME,
+			value: "Reserved-Name"
+		},
+		TOKENCOST: {
+			name: ACTION_TAG_NAME,
+			value: "Token-Cost"
+		},
+		COSTDETAILS: {
+			name: ACTION_TAG_NAME,
+			value: "Cost-Details"
+		},
+		REGISTRATIONFEES: {
+			name: ACTION_TAG_NAME,
+			value: "Registration-Fees"
+		},
+		RETURNEDNAMES: {
+			name: ACTION_TAG_NAME,
+			value: "Returned-Names"
+		},
+		RETURNEDNAME: {
+			name: ACTION_TAG_NAME,
+			value: "Returned-Name"
+		},
+		ALLOWDELEGATES: {
+			name: ACTION_TAG_NAME,
+			value: "Allow-Delegates"
+		},
+		DISALLOWDELEGATES: {
+			name: ACTION_TAG_NAME,
+			value: "Disallow-Delegates"
+		},
+		DELEGATIONS: {
+			name: ACTION_TAG_NAME,
+			value: "Delegations"
+		},
+		//
+		RECORD_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Record-Notice"
+		},
+		BUYNAME_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Buy-Name-Notice"
+		},
+		UPGRADENAME_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Upgrade-Name-Notice"
+		},
+		EXTENDLEASE_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Extend-Lease-Notice"
+		},
+		INCREASEUNDERNAMELIMIT_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Increase-Undername-Limit-Notice"
+		},
+		REASSIGNNAME_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Reassign-Name-Notice"
+		},
+		RETURNEDNAME_NOTICE: {
+			name: ACTION_TAG_NAME,
+			value: "Returned-Name-Notice"
+		},
+		//
+		NAME: (value: string) => {
+			return { name: NAME_TAG_NAME, value: value }
+		},
+	} satisfies TagRecord<Record<string, Tag | ((value: string) => Tag)>>
+} as const
