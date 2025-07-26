122a2bbcf76c17034f1a9afa9be21dfee44f0285
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-07-26 12:48:31 -0400

    docs (#217)
    Co-authored-by: Allan <31974198+BrewDogDev@users.noreply.github.com>

--- src/utils/class-interfaces/IAutoconfiguration.ts
+++ src/utils/class-interfaces/IAutoconfiguration.ts
@@ -5,6 +5,7 @@ export interface IAutoconfiguration {
 	 * Creates a pre-configured instance of the class.
 	 * This is the recommended way to instantiate this class for most use cases.
 	 * @returns A pre-configured instance of the class.
+	 * @constructor
 	 */
 	autoConfiguration(): any | Promise<any>;
 }
