6e3af7d9dc035f74842c735eea08849d880b06de
KennySwayzee93 <111819113+KennySwayzee93@users.noreply.github.com>
2025-07-29 05:38:47 -0400

    add fields (#221)
    Co-authored-by: Allan <31974198+BrewDogDev@users.noreply.github.com>

--- src/services/ario/rewind-service/abstract/responseTypes.ts
+++ src/services/ario/rewind-service/abstract/responseTypes.ts
@@ -1,5 +1,6 @@
 import { Observable } from "rxjs";
 import { IStateNoticeEvent, IReassignNameNoticeEvent, IReleaseNameNoticeEvent, IApprovePrimaryNameNoticeEvent, IRemovePrimaryNamesNoticeEvent, ICreditNoticeEvent, IDebitNoticeEvent, IBuyNameEvent, IExtendLeaseEvent, IIncreaseUndernameEvent, IReassignNameEvent, IRecordEvent, IReturnedNameEvent, IUpgradeNameEvent, ISetRecordEvent } from "../events";
+import { AntRecord } from "../../../../models";
 
 export interface ARNameDetail {
 	name: string,
@@ -9,7 +10,13 @@ export interface ARNameDetail {
 	processId: string,
 	controllers: string[],
 	owner: string,
-	ttlSeconds: number
+	ttlSeconds: number,
+	logoTxId: string
+	records: Record<string, AntRecord>,
+	targetId: string,
+	undernameLimit: number,
+	leaseDuration: string,
+	expiryDate: Date
 }
 
 export type AllARNameEventsType = {
