7bb2406e87df1ba81e1d8d33cc44423fb3f67f26
Allan <31974198+BrewDogDev@users.noreply.github.com>
2025-09-14 19:15:50 -0400

    pushing hotfix for export (#259) fix #259

--- src/services/autonomous-finance/flp-data-service/flpServices.ts
+++ src/services/autonomous-finance/flp-data-service/flpServices.ts
@@ -1,6 +1,6 @@
 import { FLPDataService } from './FLPDataService';
 import { PROCESS_IDS } from '../../../constants';
-import { IFLPDataSercvice } from './abstract';
+import { IFLPDataService } from './abstract';
 
 /**
  * Pre-configured FLP Data Service instances for different Fair Launch Processes
@@ -10,4 +10,4 @@ import { IFLPDataSercvice } from './abstract';
 /**
  * Pre-configured FLP Data Service for the Game Fair Launch Process
  */
-export const GameFLPDataService: IFLPDataSercvice = FLPDataService.autoConfiguration(PROCESS_IDS.AUTONOMOUS_FINANCE.FAIR_LAUNCH_PROCESSES.GAME);
+export const GameFLPDataService: IFLPDataService = FLPDataService.autoConfiguration(PROCESS_IDS.AUTONOMOUS_FINANCE.FAIR_LAUNCH_PROCESSES.GAME);
